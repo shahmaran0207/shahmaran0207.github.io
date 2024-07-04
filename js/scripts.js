357aa3be616fbe5b960ba37ce6158db4aae8111a
shahmaran0207 <shahmaran0207@naver.com>
2024-07-04 18:54:30 +0900

    portfolio

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,7 @@
-/*!
-* Start Bootstrap - Creative v7.0.7 (https://startbootstrap.com/theme/creative)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+
 
 window.addEventListener('DOMContentLoaded', event => {
 
-    // Navbar shrink function
     var navbarShrink = function () {
         const navbarCollapsible = document.body.querySelector('#mainNav');
         if (!navbarCollapsible) {
